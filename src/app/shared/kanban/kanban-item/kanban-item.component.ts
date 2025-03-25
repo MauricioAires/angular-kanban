37db90725ed032beba748386c868432767242be9
maf.consultoria <maf.consultoria@wefin.com.br>
2025-03-24 21:58:31 -0300

    :sparkles: create kanban item component

---
+++ src/app/shared/kanban/kanban-item/kanban-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, input } from '@angular/core';
+import { KanbanItem } from '../interfaces/kanban.interface';
+
+const statusMapperColors = {
+  Medium: 'bg-yellow-200',
+  High: 'bg-red-200',
+  Low: 'bg-green-200',
+};
+
+const statusMapperText = {
+  Medium: 'Media',
+  High: 'Alta',
+  Low: 'Baja',
+};
+
+@Component({
+  selector: 'app-kanban-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-item.component.html',
+  styleUrl: './kanban-item.component.scss',
+})
+export class KanbanItemComponent {
+  item = input.required<KanbanItem>();
+
+  color = computed(() => {
+    return statusMapperColors[this.item().priority];
+  });
+
+  statusText = computed(() => {
+    return statusMapperText[this.item().priority];
+  });
+}
