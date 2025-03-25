fcd41e32e89451127344563a0488b8802643060b
maf.consultoria <maf.consultoria@wefin.com.br>
2025-03-24 21:59:05 -0300

    :sparkles: create kanban column item

---
+++ src/app/shared/kanban/kanban-column/kanban-column.component.ts
@@ -0,0 +1,11 @@
+import { CdkDragHandle } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-column',
+  standalone: true,
+  imports: [],
+  templateUrl: './kanban-column.component.html',
+  styleUrl: './kanban-column.component.scss',
+})
+export class KanbanColumnComponent {}
